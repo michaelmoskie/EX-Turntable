5bc6d920b1fa9efd74bed1f8a0bcaeb0744f3306
Michael Moskie <michael.moskie@gmail.com>
2024-09-04 00:11:20 -0400

    Add stepper pins to atmega3208_pins.h

--- atmega3208_pins.h
+++ atmega3208_pins.h
@@ -7,4 +7,9 @@ const uint8_t relay1Pin = PIN_PD7;      // Control pin for relay 1.
 const uint8_t relay2Pin = PIN_PD6;      // Control pin for relay 2.
 const uint8_t ledPin = PIN_PD5;         // Pin for LED output.
 const uint8_t accPin = PIN_PD4;         // Pin for accessory output.
+
+const uint8_t STEPPER_PIN_0 = PIN_PA7;
+const uint8_t STEPPER_PIN_1 = PIN_PC0;
+const uint8_t STEPPER_PIN_2 = PIN_PC1;
+const uint8_t STEPPER_PIN_3 = PIN_PC2;
 #endif
